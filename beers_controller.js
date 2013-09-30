a2f670b30e582000c00d1501b96ce36288dfe962
David Moore <eroomd@gmail.com>
2013-09-30 14:50:54 -0500

    Added in a basic Angular controller to hold the beer information for now.
    TODO:: Create a way to update the controller from a database, or at least write to the file system to keep the beer list saved when the server is rebooted.

---
+++ beers_controller.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/* Controllers */
+
+function KegListController($scope) {
+	$scope.kegs = [
+		{ "name": "Beer Name",
+		  "longName": "Long Beer NAME",
+		  "style": "Style",
+		  "brewery": "Brewery Name",
+		  "abv": "0",
+		  "ibu": "0",
+		  "srm_lovibond": "0",
+		  "Status": "0" },
+		{ "name": "Beer Name 2",
+		  "longName": "Long Beer NAME",
+		  "style": "Style",
+		  "brewery": "Brewery Name",
+		  "abv": "0",
+		  "ibu": "0",
+		  "srm_lovibond": "0",
+		  "Status": "0" },
+		{ "name": "Beer Name 3",
+		  "longName": "Long Beer NAME",
+		  "style": "Style",
+		  "brewery": "Brewery Name",
+		  "abv": "0",
+		  "ibu": "0",
+		  "srm_lovibond": "0",
+		  "Status": "0" },
+		{ "name": "Beer Name 4",
+		  "longName": "Long Beer NAME",
+		  "style": "Style",
+		  "brewery": "Brewery Name",
+		  "abv": "0",
+		  "ibu": "0",
+		  "srm_lovibond": "0",
+		  "Status": "0" },
+		{ "name": "Beer Name 5",
+		  "longName": "Long Beer NAME",
+		  "style": "Style",
+		  "brewery": "Brewery Name",
+		  "abv": "0",
+		  "ibu": "0",
+		  "srm_lovibond": "0",
+		  "Status": "0" }
+	];
+}
