846d8f071c6896ab254af782e0eed8499a9f5730
David Moore <eroomd@gmail.com>
2013-10-14 08:53:38 -0500

    Lovibond color now displaying correctly.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,12 +1,12 @@
 angular.module('kegerfaceFilters', []).filter('lovibondColor', function() {
-    return function( input ) {
-        //NOTE: lovibondData from controller is not here, so can't compare or loop.
-        for ( lovibondData in srmLovibondRanges ) {
-            if ( input <= lovibondData.srmLovibond ) {
-                return lovibondData.beerColor;
-            }
-        }
-
-        return input;
-    };
+//    return function( input ) {
+//        //NOTE: lovibondData from controller is not here, so can't compare or loop.
+//        for ( lovibondData in srmLovibondRanges ) {
+//            if ( input <= lovibondData.srmLovibond ) {
+//                return lovibondData.beerColor;
+//            }
+//        }
+//
+//        return input;
+//    };
 });
--- app/js/keg_controller.js
+++ app/js/keg_controller.js
@@ -12,16 +12,19 @@ function KegListController($scope, $http) {
         $scope.srmLovibondRanges = data;
     });
 
+    $scope.lovibondUIColor = function ( lbColor ) {
+        if ( $scope.srmLovibondRanges != undefined ) {
+            for ( var i = 0; i < $scope.srmLovibondRanges.length; i++ ) {
+                if ( parseInt( lbColor ) <= $scope.srmLovibondRanges[ i ].srmLovibond ) {
+                    return $scope.srmLovibondRanges[ i ].beerColor;
+                }
+            }
+            
+            //Not under any current Lovibond range, so must be really dark.
+            return $scope.srmLovibondRanges[ $scope.srmLovibondRanges.length - 1 ].beerColor;
+        }
+        
+        return '#000000';
+    };
     
-//    kegerfaceApp.controller('KegListController', function KegListController($scope, $routeParams) {
-//        $scope.foo = $routeParams.foo;
-//    });
 }
-
-//function LovibondController( $scope, $http ) {
-//    "use strict";
-//    
-//    $http.get('dataStore/srmLovibond.json').success( function ( data ) {
-//        $scope.srmLovibond = data;
-//    });
-//}
