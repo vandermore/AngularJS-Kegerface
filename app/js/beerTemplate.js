f4ef24ba44f4c8c43c3d4e07e3fa37401fbe2625
David Moore <eroomd@gmail.com>
2013-10-14 15:36:16 -0500

    Working on getting other controllers set up, and multiple services.

---
+++ app/js/beerTemplate.js
@@ -0,0 +1,4 @@
+$scope.templates =
+    [ { name: 'template1.html', url: 'template1.html'}
+    , { name: 'template2.html', url: 'template2.html'} ];
+  $scope.template = $scope.templates[0];
--- app/js/keg_controller.js
+++ app/js/keg_controller.js
@@ -4,27 +4,27 @@ var kegerfaceController = angular.module( 'kegerfaceController', [] );
 var lovibondController = angular.module( 'lovibondController', [] );
 
 kegerfaceController.controller( 'KegListController', ['$scope', 'Kegerator',
-													  function($scope, Kegerator) {
+													  function($scope, Kegerator ) {
 														  $scope.kegs = Kegerator.query();
 														  $scope.orderProp = 'name';
 													  }]);
 
-lovibondController.controller( 'LovibondController', ['$scope', 'Lovibond',
+lovibondController.controller( 'LovibondListController', ['$scope', 'Lovibond',
 													  function( $scope, Lovibond ) {
 															$scope.srmLovibondRanges = Lovibond.query();
 														  
-															$scope.lovibondUIColor = function ( lbColor ) {
-																if ( $scope.srmLovibondRanges != undefined ) {
-																	for ( var i = 0; i < $scope.srmLovibondRanges.length; i++ ) {
-																		if ( parseInt( lbColor ) <= $scope.srmLovibondRanges[ i ].srmLovibond ) {
-																			return $scope.srmLovibondRanges[ i ].beerColor;
-																		}
-																	}
-																	
-																	//Not under any current Lovibond range, so must be really dark.
-																	return $scope.srmLovibondRanges[ $scope.srmLovibondRanges.length - 1 ].beerColor;
-																}
-																
-																return '#000000';
-															};
+//															$scope.lovibondUIColor = function ( lbColor ) {
+//																if ( $scope.srmLovibondRanges != undefined ) {
+//																	for ( var i = 0; i < $scope.srmLovibondRanges.length; i++ ) {
+//																		if ( parseInt( lbColor ) <= $scope.srmLovibondRanges[ i ].srmLovibond ) {
+//																			return $scope.srmLovibondRanges[ i ].beerColor;
+//																		}
+//																	}
+//																	
+//																	//Not under any current Lovibond range, so must be really dark.
+//																	return $scope.srmLovibondRanges[ $scope.srmLovibondRanges.length - 1 ].beerColor;
+//																}
+//																
+//																return '#000000';
+//															};
 													  }]);
