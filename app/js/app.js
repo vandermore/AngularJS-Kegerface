0ebfab78d95b4428bd979a72a90e35b8feb0db8f
David Moore <eroomd@gmail.com>
2013-10-15 12:55:52 -0500

    Added in comments.

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 /* kegerfaceApp is the app name, and all of the strings in the array are dependencies for the app.*/
 var kegerfaceApp = angular.module('kegerfaceApp', ['kegerfaceController', 'lovibondController', 'kegerfaceFilters', 'kegerfaceServices']);
 
+/* Configuration block. Configures the route provider for the application. We don't entirely need this one here, it's just here to learn from. */
 kegerfaceApp.config(['$routeProvider', function($routeProvider) {
       /* Super simple route provider */
       $routeProvider.
